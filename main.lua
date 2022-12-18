607a9f73594c31c70f2c667dbf7f18779dd46986
Reid <88220593+DevDodger@users.noreply.github.com>
2022-12-18 12:18:10 -0800

    Update and rename COOL.lua to main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,3 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
 local h = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local u = Instance.new("Frame")
@@ -15,20 +10,15 @@ local UICorner_3 = Instance.new("UICorner")
 local Spawn = Instance.new("TextButton")
 local UICorner_4 = Instance.new("UICorner")
 local UICorner_5 = Instance.new("UICorner")
-
---Properties:
-
 h.Name = "h"
 h.Parent = game.CoreGui
 h.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 h.Active = true
 h.Draggable = true
-
 Frame.Parent = h
 Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 Frame.Position = UDim2.new(0.289056599, 0, 0.254364073, 0)
 Frame.Size = UDim2.new(0, 559, 0, 394)
-
 u.Name = "u"
 u.Parent = Frame
 u.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
@@ -36,10 +26,7 @@ u.Position = UDim2.new(0.0214669053, 0, 0.025380712, 0)
 u.Size = UDim2.new(0, 535, 0, 373)
 u.Active = true
 u.Draggable = true
-
-
 UICorner.Parent = u
-
 TextLabel.Parent = u
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
@@ -51,7 +38,6 @@ TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
-
 ServerNotifi.Name = "ServerNotifi"
 ServerNotifi.Parent = u
 ServerNotifi.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -61,9 +47,7 @@ ServerNotifi.Font = Enum.Font.SourceSansBold
 ServerNotifi.Text = "Server Notification"
 ServerNotifi.TextColor3 = Color3.fromRGB(255, 255, 255)
 ServerNotifi.TextSize = 14.000
-
 UICorner_2.Parent = ServerNotifi
-
 Credit.Name = "Credit"
 Credit.Parent = u
 Credit.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -73,9 +57,7 @@ Credit.Font = Enum.Font.SourceSansBold
 Credit.Text = "Send Hack Credit"
 Credit.TextColor3 = Color3.fromRGB(255, 255, 255)
 Credit.TextSize = 14.000
-
 UICorner_3.Parent = Credit
-
 Spawn.Name = "Spawn"
 Spawn.Parent = u
 Spawn.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -85,42 +67,31 @@ Spawn.Font = Enum.Font.SourceSansBold
 Spawn.Text = "Game Crasher"
 Spawn.TextColor3 = Color3.fromRGB(255, 255, 255)
 Spawn.TextSize = 14.000
-
 UICorner_4.Parent = Spawn
-
-UICorner_5.Parent = Frame
-
--- Scripts:
-
-local function EWIIPG_fake_script() -- ServerNotifi.Script 
+local function EWIIPG_fake_script() 
 	local script = Instance.new('Script', ServerNotifi)
-
 	script.Parent.MouseButton1Down:connect(function()
 		local disable = game.ReplicatedStorage.EnemyContainer.Enemy_CQC.Saude.Stance
 		disable:FireAllClients()
 	end)
-	
 end
 coroutine.wrap(EWIIPG_fake_script)()
-local function BSFYC_fake_script() -- Credit.Script 
+local function BSFYC_fake_script() 
 	local script = Instance.new('Script', Credit)
-
 	script.Parent.MouseButton1Down:connect(function()
 		local disable = game.ReplicatedStorage.EnemyContainer.Enemy_CQC.Saude.Position
 		disable:FireAllClients()
 	end)
-	
 end
 coroutine.wrap(BSFYC_fake_script)()
-local function EDZUKT_fake_script() -- Spawn.Script 
+local function EDZUKT_fake_script() 
 	local script = Instance.new('Script', Spawn)
 
 	script.Parent.MouseButton1Click:connect(function(GetCar)
-		Mod = game.ServerStorage.TTPPluginAdmins.LoadUsers     -- Change test to whatever you have named the item you want to spawn.
+		Mod = game.ServerStorage.TTPPluginAdmins.LoadUsers
 		clone = Mod:clone()
 		clone.Parent = workspace
 		clone:MakeJoints()
 	end)
-	
 end
 coroutine.wrap(EDZUKT_fake_script)()
